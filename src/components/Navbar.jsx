cf10ad77c80d0194143eb5fe16377b1903d58d6a
tejayy <tbj456@gmail.com>
2025-03-22 21:30:06 +0530

    minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,9 @@ export default function Navbar() {
         <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
             <img className="h-10 w-10 mr-2" src={logo} alt="logo" />
-            <span className="text-xl tracking-tight">VirtualR</span>
+            <a href="#" className="text-xl tracking-tight cursor-pointer">
+              VirtualR
+            </a>
           </div>
           <ul className="hidden lg:flex ml-14 space-x-12">
             {navItems.map((item, index) => (
